c6f4715b879018059eac028d8ca2b38a7b115277
Prashanth Sreepathi <prashanthsreepathi@Prashanths-MBP.midcoip.net>
2020-10-12 21:50:23 -0500

    added product filters on products page

---
+++ src/components/products/CheckboxFilter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="px-2 py-3">
+        <div class="flex items-center justify-between pb-2">
+            <p class="font-medium">{{name}}</p>
+            <div class="cursor-pointer" @click="isOpen = !isOpen">
+                <svg v-if="!isOpen" class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1200 1200"><path d="M600.002 210.605L421.285 389.336L0 810.559l178.721 178.836l421.281-421.341l421.281 421.341L1200 810.559L778.733 389.336L600.002 210.605z" fill="#626262"/></svg>
+                <svg v-else class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1200 1200"><path d="M600.006 989.352l178.709-178.709L1200 389.357l-178.732-178.709L600.006 631.91L178.721 210.648L0 389.369l421.262 421.262l178.721 178.721h.023z" fill="#626262"/></svg>
+            </div>
+        </div>
+        <div v-if="isOpen">
+            <label v-for="filter in filters" :key="filter" class="block">
+                <input class="mr-3 leading-tight" type="checkbox">
+                <span class="text-sm">
+                    {{filter}}
+                </span>
+            </label>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['filters', 'name'],
+    data() {
+        return {
+            isOpen: true
+        }
+    },
+}
+</script>
--- src/views/TheShop.vue
+++ src/views/TheShop.vue
@@ -1,3 +1,32 @@
 <template>
-    <h2>The Shop Component</h2>
+    <div class="bg-gray-200 flex px-16 py-2">
+        <div class="w-1/5 mr-2 bg-white shadow-lg rounded divide-y">
+            <h2 class="font-semibold text-lg px-2 py-3">Filters</h2>
+            <checkbox-filter :filters="categories" name="Categories"></checkbox-filter>
+            <checkbox-filter :filters="brands" name="Brands"></checkbox-filter>
+            <checkbox-filter :filters="gender" name="Gender"></checkbox-filter>
+            <checkbox-filter :filters="ratings" name="Customer Ratings"></checkbox-filter>
+            <checkbox-filter :filters="discounts" name="Discount"></checkbox-filter>
+        </div>
+        <div class="w-4/5 bg-white shadow-lg rounded p-2">
+            <h2>Product List</h2>
+        </div>
+    </div>
 </template>
+<script>
+import CheckboxFilter from './../components/products/CheckboxFilter.vue'
+export default {
+    components: {
+        CheckboxFilter
+    },
+    data() {
+        return {
+            brands: ['Adidas','Puma','Nike','Reebok','Jack & jones','Tommy Hilfiger'],
+            ratings: ['4 star & above', '3 start & above', '2 start & above', '1 start & above'],
+            discounts: ['70% & more', '60% & more', '50% & more', '40% & more', '30% & more', '30% & below'],
+            gender: ['Men', 'Women'],
+            categories: ['Clothing', 'Electronics', 'Shoes', 'Watches', 'TVs & Appliances', 'Home & Furniture', 'Accesories']
+        }
+    }
+}
+</script>
